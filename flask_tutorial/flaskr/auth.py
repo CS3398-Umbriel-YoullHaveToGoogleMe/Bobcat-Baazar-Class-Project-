abedc087d5e9cfa92735d5e4b47e3165188e2bc8
nmanasiya <42896536+nmanasiya@users.noreply.github.com>
2018-10-04 16:01:45 -0500

    Added name to the input element
    made changes to login .html, added name attribute to input element.

--- flask_tutorial/flaskr/auth.py
+++ flask_tutorial/flaskr/auth.py
@@ -12,8 +12,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['uname']
+        password = request.form['pass']
         db = get_db()
         error = None
 

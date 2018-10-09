0b9ec4a0d6980cf3f84a3bfac9be13ad8c0fb46b
nmanasiya <42896536+nmanasiya@users.noreply.github.com>
2018-10-09 16:43:58 -0500

    changed the hello to index page

--- Bobcat_Bazaar/flaskr/__init__.py
+++ Bobcat_Bazaar/flaskr/__init__.py
@@ -31,9 +31,9 @@ def create_app(test_config=None):
 
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def index():
+        return 'Hello World!'
 
     from . import db
     db.init_app(app)

89c8632f70dcd4eb0c324e69ca4840619b76ead3
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-10-10 21:22:22 -0500

    changes

--- Bazaar/BobcatBazaarApp/__init__.py
+++ Bazaar/BobcatBazaarApp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -8,4 +8,13 @@ def hello():
 
 @app.route('/')
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(url_for('home'))
+
     return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')

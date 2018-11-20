ff5819b04e2f6715dec5dff12af4c404ec976089
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-11-20 14:29:04 -0600

    Deleting unneccesary lines of code.

--- Bobcat Bazaar Final/__init__.py
+++ Bobcat Bazaar Final/__init__.py
@@ -1,7 +1,4 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for, flash, g, session
-from werkzeug.security import check_password_hash, generate_password_hash
-#from BobcatBazaarApp.db import get_db
 from flask_pymongo import PyMongo
 import bcrypt
 
